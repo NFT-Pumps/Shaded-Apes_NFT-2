1e159e29a3d6f821ae45d51eaee707711a2be1ef
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-12-09 09:26:17 -0600

    Initial

---
+++ other/couponGenerator.js
@@ -0,0 +1,78 @@
+//npx hardhat run .\other\couponGenerator.js 
+const { ethers } = require("hardhat");
+const fs = require("fs");
+
+  
+const {
+    keccak256,
+    toBuffer,
+    ecsign,
+    bufferToHex,
+} = require("ethereumjs-utils");
+
+let signerPvtKey1 = process.env.SigPK;
+
+//const signerPvtKey = Buffer.from(signerPvtKey1.substring(2,66), "hex");
+const signerPvtKey = Buffer.from(signerPvtKey1, "hex");
+
+
+let coupons = {};
+
+async function getClaimCodes() {
+    //const [owner, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20] = await ethers.getSigners();
+
+    let presaleAddresses = [
+        { address : '0x9Dc7D54E10A199C00176FFA68459fD9b15A706c4', qty : 5},
+        { address : '0xCd43AdcB61949ab14D3f4574BFbDA53d46389715', qty : 1}       
+    ]      
+    
+    function createCoupon(hash, signerPvtKey) {
+        return ecsign(hash, signerPvtKey);
+    }
+    
+    function generateHashBuffer(typesArray, valueArray) {
+        return keccak256(
+            toBuffer(ethers.utils.defaultAbiCoder.encode(typesArray,
+                valueArray))
+        );
+    }
+
+    function serializeCoupon(coupon) {
+        return {
+            r: bufferToHex(coupon.r),
+            s: bufferToHex(coupon.s),
+            v: coupon.v
+        };
+    }
+
+    for (let i = 0; i < presaleAddresses.length; i++) {
+        const userAddress = ethers.utils.getAddress(presaleAddresses[i].address);
+        const hashBuffer = generateHashBuffer(
+            ["uint256", "address"],
+            [presaleAddresses[i].qty, userAddress]
+        );
+        const coupon = createCoupon(hashBuffer, signerPvtKey);
+
+        coupons[userAddress] = {
+            q : presaleAddresses[i].qty,
+            whitelistClaimPass: serializeCoupon(coupon)
+        };
+    }
+    // HELPER FUNCTIONS
+    
+    // get the Console class
+    const { Console } = require("console");
+    // get fs module for creating write streams
+    const fs = require("fs");
+
+    // make a new logger
+    const myLogger = new Console({
+    stdout: fs.createWriteStream("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266-signed-coupons.txt"),
+    stderr: fs.createWriteStream("errStdErr.txt"),
+    });
+
+    myLogger.log(coupons);
+   
+}
+
+getClaimCodes()
---
+++ scripts/deployArgs.js
@@ -0,0 +1,26 @@
+module.exports = [
+ 'Base Contract',
+ 'TKCNFT',
+ 
+    '0x9C3f261e2cc4C88DfaC56A5B46cdbf767eE2f231',
+    '0xc01aC67ba14F13Af4fCD5b4ba3784F39a703cda0',
+    'https://public-pre-ipfs.s3.amazonaws.com/BaseContract/json/',
+    'https://public-pre-ipfs.s3.amazonaws.com/BaseContract/assets/KreamerReveal.json',
+
+    [
+        ethers.utils.getAddress('0xd708BA3dA864cC6E8596d3E53d3468bAB54Be9b0'),
+        ethers.utils.getAddress('0x8912e40d291693C7A86C418D52b46766780C6989'),
+        ethers.utils.getAddress('0x6b3ba9F0475C646FDaF2Bc207C285a485B88A747'),
+        ethers.utils.getAddress('0xE4cD887b35C7a7941EEbE8336c6Bfc5d1E9f7bfA'),
+        ethers.utils.getAddress('0x744193D5f234183FC821DfB0ACBb6eB8a1792Bc2'),
+        ethers.utils.getAddress('0x2E00F4Dd34c0A9aab2cd64B046469336A4914870')
+    ],
+    [
+        26,
+        12,
+        10,
+        6,
+        6,
+        40
+    ]
+]
